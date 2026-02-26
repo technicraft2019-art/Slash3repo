949907240b202eb417a4af17b18ec5e9b6aba369
Charles Poole <technicraft2019@gmail.com>
2026-02-26 04:42:14 -0500

    Finished commenting entire Item class with no errors.

--- Source/Slash3/Public/Item.h
+++ Source/Slash3/Public/Item.h
@@ -1,23 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+// pragma once tells the compiler to include this header file only once in a single compilation, 
+// preventing duplicate definitions and improving compilation speed.
 #pragma once
 
+// This is a list of include directives that bring in the necessary header files for this class.
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+// A Macro that tells Unreal Engine to generate code for this class, enabling features like reflection and serialization. 
 UCLASS()
+
+// Creates a new class called AItem that inherits from AActor.
 class SLASH3_API AItem : public AActor
 {
+	/* This macro is used to generate boilerplate code for the AItem class.
+	It is necessary for Unreal Engine's reflection system, 
+	which allows the engine to recognize and interact with the class in various ways, 
+	such as exposing it to Blueprints, enabling serialization, and more. */
 	GENERATED_BODY()
 	
 public:	
-	// Sets default constructor values for this actor's properties
+	// Creates a constructor for the AItem class.
 	AItem();
-	// Called every frame
+	// Called every frame (this is the blueprint equivalent of the Event Tick in Unreal Engine).
 	virtual void Tick(float DeltaTime) override;
 
 protected:
-	// Called when the game starts or when spawned
+	// Creates a function override for the BeginPlay() function.
 	virtual void BeginPlay() override;
 };
