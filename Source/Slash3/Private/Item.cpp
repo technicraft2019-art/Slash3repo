9bb9976b23e72489be98b3c972c34895ae140872
Charles Poole <technicraft2019@gmail.com>
2026-02-26 21:03:12 -0500

    Finished Lecture series on Debug Tools and Macro creation.

--- Source/Slash3/Private/Item.cpp
+++ Source/Slash3/Private/Item.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "Item.h"
-#include "Slash3/Slash3.h"
+#include "Slash3/DebugMacros.h"
 
 // Sets constructor default values
 AItem::AItem()
